b17776a0c1ae41b7e06b1d8c07adfa10603c25b4
linrubin8 <707486792@qq.com>
2016-10-01 21:12:35 +0800

    linrubin-视图配置

--- LB.SQL/SQLConfig/SysViewType.sql
+++ LB.SQL/SQLConfig/SysViewType.sql
@@ -1,7 +1,30 @@
-﻿delete dbo.SysViewType
-insert dbo.SysViewType( SysViewType, SysViewName ) values('100','DBUser') 
-insert dbo.SysViewType( SysViewType, SysViewName) values('101','DbSystemConst') 
-insert dbo.SysViewType( SysViewType, SysViewName) values('102','Db_v_UserPermission') 
-insert dbo.SysViewType( SysViewType, SysViewName) values('103','DbPermission') 
-insert dbo.SysViewType( SysViewType, SysViewName) values('104','DbPermissionData') 
-insert dbo.SysViewType( SysViewType, SysViewName) values('105','DbReportTemplate') 
+﻿
+if not exists(select 1 from SysViewType where SysViewType='100')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName ) values('100','DBUser') 
+end
+go
+if not exists(select 1 from SysViewType where SysViewType='101')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName) values('101','DbSystemConst') 
+end
+go
+if not exists(select 1 from SysViewType where SysViewType='102')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName) values('102','Db_v_UserPermission') 
+end
+go
+if not exists(select 1 from SysViewType where SysViewType='103')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName) values('103','DbPermission') 
+end
+go
+if not exists(select 1 from SysViewType where SysViewType='104')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName) values('104','DbPermissionData') 
+end
+go
+if not exists(select 1 from SysViewType where SysViewType='105')
+begin
+	insert dbo.SysViewType( SysViewType, SysViewName) values('105','DbReportTemplate') 
+end
